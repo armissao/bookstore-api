dc0e58349e63b218d70d5b39b8eaa9bdee42f365
Andre Missão <armissao@gmail.com>
2021-07-05 09:58:28 -0300

    Comite Inicial

---
+++ src/main/java/br/com/nbtreiner/apibookstore/ApibookstoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.nbtreiner.apibookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApibookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nbtreiner/apibookstore/ApibookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nbtreiner.apibookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApibookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
